48101243eaf0b9fa9e0423fddfeb9cc7b62b42ac
zeruel11 <the.zero0110@gmail.com>
2018-06-01 04:56:24 +0700

    jscalc init

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index'),
+  output: {
+    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      include: [
+        path.resolve(__dirname, 'src')
+      ],
+      exclude: [
+        path.resolve(__dirname, 'node_modules'),
+        path.resolve(__dirname, 'bower_components')
+      ],
+      use: [{ loader: 'babel-loader' }],
+      // query: {
+      //   presets: ['es2015']
+      // }
+    },
+    {
+      test: /\.css$/,
+      use: [{
+        loader: 'style-loader'
+      },{
+        loader: 'css-loader'
+      }],
+    }]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: 'index.html'
+    }),
+    new CleanWebpackPlugin(['dist'])
+  ],
+  // resolve: {
+  //   extensions: ['.json', '.js', '.jsx', '.css']
+  // },
+  // devtool: 'source-map',
+  mode: 'development',
+  devServer: {
+    // publicPath: path.join('/dist/')
+    host: 'localhost',
+    port: 3000,
+    open: true
+  }
+};
