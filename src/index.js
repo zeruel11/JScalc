9a7cafd40dfd55229d591e372712ca4e6186bb29
zeruel11 <the.zero0110@gmail.com>
2018-06-01 13:12:42 +0700

    bugfix

--- src/index.js
+++ src/index.js
@@ -4,17 +4,34 @@ const calculator = document.querySelector('.calculator')
 const display = document.querySelector('.calculator__display')
 const keys = calculator.querySelector('.calculator__keys')
 
+const calculate = (n1, operator, n2) => {
+  let result = ''
+  if (operator === 'add') {
+    result = parseFloat(n1) + parseFloat(n2)
+  } else if (operator === 'subtract') {
+    result = parseFloat(n1) - parseFloat(n2)
+  } else if (operator === 'multiply') {
+    result = parseFloat(n1) * parseFloat(n2)
+  } else if (operator === 'divide') {
+    result = parseFloat(n1) / parseFloat(n2)
+  }
+  return result
+}
+
 keys.addEventListener('click', e => {
   if (e.target.matches('button')) {
     const key = e.target
-    // remove operator visual state
-    Array.from(key.parentNode.children)
-      .forEach(k => k.classList.remove('is-depressed'))
     const action = key.dataset.action
     const keyContent = key.textContent
     const displayedNum = display.textContent
     const previousKeyType = calculator.dataset.previousKeyType
+
+    // remove operator visual state
+    Array.from(key.parentNode.children)
+      .forEach(k => k.classList.remove('is-depressed'))
+
     if (!action) {
+      calculator.dataset.previousKeyType = ''
       if (displayedNum === '0' || previousKeyType === 'operator') {
         display.textContent = keyContent
       } else {
@@ -44,20 +61,6 @@ keys.addEventListener('click', e => {
       const firstValue = calculator.dataset.firstValue
       const operator = calculator.dataset.operator
       const secondValue = displayedNum
-
-      const calculate = (n1, operator, n2) => {
-        let result = ''
-        if (operator === 'add') {
-          result = parseFloat(n1) + parseFloat(n2)
-        } else if (operator === 'subtract') {
-          result = parseFloat(n1) - parseFloat(n2)
-        } else if (operator === 'multiply') {
-          result = parseFloat(n1) * parseFloat(n2)
-        } else if (operator === 'divide') {
-          result = parseFloat(n1) / parseFloat(n2)
-        }
-        return result
-      }
       display.textContent = calculate(firstValue, operator, secondValue)
       // console.log('equal key!')
     }
